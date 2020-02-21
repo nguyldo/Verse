4ed1acd556a199cf9dc63a97cd16b396737bdc51
nguyldo <nguyldo@gmail.com>
2020-02-21 09:33:19 -0800

    save uploaded files in media folder

--- verse/dataParser/views.py
+++ verse/dataParser/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import Context, loader
+from django.core.files.storage import FileSystemStorage
 #from pprint import pprint
 
 # Create your views here.
@@ -12,6 +13,8 @@ def index(request):
 def upload(request):
     if request.method == "POST":
         uploadedFile = request.FILES["document"]
-        #print(uploadedFile.read())
-        print(uploadedFile.read().decode("utf-8"))
+        # print(uploadedFile.read())
+        # print(uploadedFile.read().decode("utf-8"))  <-- THIS LINE PRINTS OUT THE UPLOADED FILE
+        fss = FileSystemStorage()
+        fss.save(uploadedFile.name, uploadedFile)
     return render(request, "upload.html")
--- verse/verse/settings.py
+++ verse/verse/settings.py
@@ -32,6 +32,9 @@ ALLOWED_HOSTS = []
 
 # Application definition
 
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
+
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
