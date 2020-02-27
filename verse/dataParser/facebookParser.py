7cfbdc0936bb3032e25cfc6f7116ff1154e0a1b2
Lisa <le63@purdue.edu>
2020-02-26 19:39:09 -0500

    facebook parser now handles posts, profile info, and advertising info

--- verse/dataParser/facebookParser.py
+++ verse/dataParser/facebookParser.py
@@ -1,9 +1,11 @@
 import os
 from os import path
-
 import string
+import json
+from pprint import pprint
 
-# Code from https://stackoverflow.com/a/234329
+# Function to traverse *some_dir* with a specified *level* of recursive depth
+# From https://stackoverflow.com/a/234329
 def walklevel(some_dir, level=1):
     some_dir = some_dir.rstrip(os.path.sep)
     assert os.path.isdir(some_dir)
@@ -14,43 +16,85 @@ def walklevel(some_dir, level=1):
         if num_sep + level <= num_sep_this:
             del dirs[:]
 
-def 
-# Define dictionary to map 
-Dict = {}
-print(Dict)
+# Function to extract json data from the directory at *pathName*,
+# concatenate *rootDir* and *fileOfInterest*, 
+# and return a key and value pair 
+def jsonFileToString(pathName, rootDir, fileOfInterest):
+    fileName = pathName + "/" + rootDir + "/" + fileOfInterest
 
-# Define tuple to store all possible directory names in the data dump
-categories = ("about_you", "ads_and_businesses", "apps_and_websites", "comments", 
-            "events", "following_and_followers", "friends", "groups", 
-            "likes_and_reactions", "location", "marketplace", "messages", 
-            "other_activity", "pages", "payment_history", "photos_and_videos", 
-            "portal", "posts", "profile_information", "saved_items_and_collections", 
-            "search_history", "security_and_login_information", "stories", "your_places")
+    with open(fileName) as jsonFile:
+        data = json.load(jsonFile)
 
+    key = rootDir + ">" + fileOfInterest[:-5]
+    value = data
 
-#if (any("about_you" in category for category in categories)):
-#    print("about_you exists")
+    return key, value
 
-# TODO: get expected directory name passed from views.py after file has been uploaded, so the parser knows which directory to walk
+# Function to extract json data from the root directory of facebook data 
+# and input into a dictionary
+def parseFacebookData(facebookMediaRoot): 
+    # Define dictionary to map json data to
+    Dict = {}
 
-# Print directory structure under uploaded and unzipped folder
-if(path.exists("../media")):
-    print("path exists")
+    # Define tuple to store all possible root directory names in the data dump
+    rootCategories = ("about_you", "ads_and_businesses", "apps_and_websites", "comments", 
+                "events", "following_and_followers", "friends", "groups", 
+                "likes_and_reactions", "location", "marketplace", "messages", 
+                "other_activity", "pages", "payment_history", "photos_and_videos", 
+                "portal", "posts", "profile_information", "saved_items_and_collections", 
+                "search_history", "security_and_login_information", "stories", "your_places")
 
-    for root, dirs, files in walklevel("../media/unzippedFiles/facebook-lisasilmii", level=1):
-        # code from https://stackoverflow.com/a/7253830
-        root_dir = root.rsplit('/', 1)[-1]
+    # Define tuple to store root directory names of interest
+    rootCategoriesOfInterest = ("ads_and_businesses", "posts", "profile_information")
+
+    # Parse through facebook media root directory and extract json data
+    pathVerseFacebookMedia = "../media/unzippedFiles/facebook/"
+    if(path.exists(pathVerseFacebookMedia)):
+        pathName = pathVerseFacebookMedia + facebookMediaRoot
         
-        #if category is valid
-        if (any(root_dir in category for category in categories)):
-            #add category as key to dictionary and directory contents as value
+        for root, dirs, files in walklevel(pathName, level=1):
+            # from https://stackoverflow.com/a/7253830
+            rootDir = root.rsplit('/', 1)[-1]
+            
+            # if category is valid,
+            # add category as key and 
+            # directory contents as value 
+            # to dictionary
+            if any(rootDir in category for category in rootCategoriesOfInterest):
+                print(rootDir + ": ")
 
-            if not dirs:
-                Dict[root_dir] = files
-            else:
-                Dict[root_dir] = dirs
+                if rootDir == "ads_and_businesses":
+                    fileOfInterest = "advertisers_who_uploaded_a_contact_list_with_your_information.json"
 
-    print(Dict)
+                    key, value = jsonFileToString(pathName, rootDir, fileOfInterest)
+                    Dict[key] = value
 
-else: print("path does not exist")
+                elif rootDir == "posts":
+                    fileOfInterest1 = "other_people's_posts_to_your_timeline.json"
+                    key1, value1 = jsonFileToString(pathName, rootDir, fileOfInterest1)
+                    Dict[key1] = value1
 
+                    fileOfInterest2 = "your_posts_1.json"
+                    key2, value2 = jsonFileToString(pathName, rootDir, fileOfInterest2)
+                    Dict[key2] = value2
+                    
+                elif rootDir == "profile_information":
+                    fileOfInterest1 = "profile_information.json"
+                    key1, value1 = jsonFileToString(pathName, rootDir, fileOfInterest1)
+                    Dict[key1] = value1
+
+                    fileOfInterest2 = "profile_update_history.json"
+                    key2, value2 = jsonFileToString(pathName, rootDir, fileOfInterest2)
+                    Dict[key2] = value2
+        
+        #print("\n\n\nDictionary\n")
+        #print(Dict["posts>your_posts_1"])
+
+    else: print("path does not exist")
+
+def main():
+    root = "facebook-lisasilmii"
+    parseFacebookData(root)
+
+if __name__ == "__main__":
+    main()
