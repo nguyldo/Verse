8d8bf504ba92adbd81dcabec1b56fffefe5ac5fe
nguyldo <nguyldo@gmail.com>
2020-03-05 10:21:51 -0500

    initialized pipfile and front-end

--- verse/front-end/src/App.js
+++ verse/front-end/src/App.js
@@ -1,35 +1,12 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends React.Component {
   render() {
     return (
-      <button onClick="chooseFile()">Choose a file</button>
+      <button>ANALYZE YOUR DATA</button>
     )
   }
 }
-
-/* THIS IS AN ALTERNATE METHOD IN CREATING REACT COMPONENTS. I CHOSE TO USE A DIFFERENT METHOD ABOVE
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}*/
 
 export default App;
--- verse/verse/settings.py
+++ verse/verse/settings.py
@@ -43,10 +43,13 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'corsheaders',
     'dataParser'
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -54,6 +57,10 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+CORS_ORIGIN_WHITELIST = [
+    'http://localhost:3000'
 ]
 
 ROOT_URLCONF = 'verse.urls'
