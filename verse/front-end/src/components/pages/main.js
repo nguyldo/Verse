6da74e0dd679e18b7f7a8c709d303bd96e966547
JacquieLeeYu <yu790@purdue.edu>
2020-03-28 06:01:40 -0700

    Nav into main.js

--- verse/front-end/src/components/pages/main.js
+++ verse/front-end/src/components/pages/main.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 
 import axios from "axios";
-import {Link} from "react-router-dom";
+import {Link, NavLink} from "react-router-dom";
 import Header from "./../sections/header.js";
 
 export default class Main extends Component {
@@ -17,7 +17,11 @@ export default class Main extends Component {
             <h1>Welcome to Verse!</h1>
             <h3>Verse slogan here...</h3>
             <br></br>
-
+            <nav>
+              <NavLink to="/upload" activeStyle={{background:'white', color:'blue'}}>Upload</NavLink>
+              <NavLink to="/help" activeStyle={{background:'white', color:'blue'}}>Help</NavLink>
+              <NavLink to="/about" activeStyle={{background:'white', color:'blue'}}>About Us</NavLink>
+            </nav>
             <Link to="/upload" id="uploadslink">Analyze my data!</Link>
         </div>
       )
