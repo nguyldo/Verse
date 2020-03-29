c26b422b038392d9113185fdab9bc82095117b2c
JacquieLeeYu <yu790@purdue.edu>
2020-03-28 17:30:30 -0700

    Move to header.js

--- verse/front-end/src/components/pages/main.js
+++ verse/front-end/src/components/pages/main.js
@@ -17,11 +17,7 @@ export default class Main extends Component {
             <h1>Welcome to Verse!</h1>
             <h3>Verse slogan here...</h3>
             <br></br>
-            <nav>
-              <NavLink to="/upload" activeStyle={{background:'white', color:'blue'}}>Upload</NavLink>
-              <NavLink to="/help" activeStyle={{background:'white', color:'blue'}}>Help</NavLink>
-              <NavLink to="/about" activeStyle={{background:'white', color:'blue'}}>About Us</NavLink>
-            </nav>
+            
             <Link to="/upload" id="uploadslink">Analyze my data!</Link>
         </div>
       )
--- verse/front-end/src/components/sections/header.js
+++ verse/front-end/src/components/sections/header.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 
 import axios from "axios";
-import {Link} from "react-router-dom";
+import {Link, NavLink} from "react-router-dom";
 import "./../../css/header.css";
 
 export default class Header extends Component {
@@ -14,6 +14,11 @@ export default class Header extends Component {
       return(
         <div>
             <Link to="/" id="link"><h1 id="header">VERSE</h1></Link>
+            <nav>
+              <NavLink to="/upload" activeStyle={{background:'white', color:'blue'}}>Upload</NavLink>
+              <NavLink to="/help" activeStyle={{background:'white', color:'blue'}}>Help</NavLink>
+              <NavLink to="/about" activeStyle={{background:'white', color:'blue'}}>About Us</NavLink>
+            </nav>
         </div>
       )
     }
